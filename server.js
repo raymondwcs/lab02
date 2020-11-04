4541d5bb26f041b55201fda73b8c26acf5688e5b
Raymond SO <rso@ouhk.edu.hk>
2020-11-04 19:29:46 +0800

    used more ES6 syntax

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const ObjectId = require('mongodb').ObjectID;
 const url = '';  // MongoDB Atlas Connection URL
 const dbName = 'test'; // Database Name
 
-const countRestaurants = function(db, callback) {
+const countRestaurants = (db, callback) => {
 	var collection = db.collection('restaurants');
-	collection.countDocuments(function(err,count) {
+	collection.countDocuments((err,count) => {
 		assert.equal(null,err);
 		console.log(`There are ${count} documents in the restuarant collection`);
 	})
@@ -15,13 +15,13 @@ const countRestaurants = function(db, callback) {
 
 const client = new MongoClient(url);
 
-client.connect(function(err) {
+client.connect((err) => {
    assert.equal(null,err);
    console.log(`Connected successfully to ${url}`);
 
    const db = client.db(dbName);
 
-   countRestaurants(db, function() {
+   countRestaurants(db, () => {
 	client.close();
    })
 })
